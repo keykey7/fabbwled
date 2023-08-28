9d8db893cfcaa3b2de433dfe327296e0d0351247
Oliver <75665514+Oliver-was-taken@users.noreply.github.com>
2023-08-28 16:23:07 +0200

    setup frontend

---
+++ fabbwled-frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/create" />
+      <Route path="/game" />
+    </Routes>
+  );
+}
+
+export default App;
