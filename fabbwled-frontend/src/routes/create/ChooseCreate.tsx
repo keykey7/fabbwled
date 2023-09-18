3456d384fcac6d1d4e34dd6b9b644bc3c1e99827
Oliver <75665514+Oliver-was-taken@users.noreply.github.com>
2023-09-18 17:02:37 +0200

    style for character creation options

---
+++ fabbwled-frontend/src/routes/create/ChooseCreate.tsx
@@ -0,0 +1,10 @@
+import './choose-create.scss';
+
+export default function ChooseCreate() {
+    return (
+        <div className="choose_create_container">
+            <button>Create own character</button> {/* TODO OLIVER: add route character creation */}
+            <button>Play with existing character</button>
+        </div>
+    );
+}
