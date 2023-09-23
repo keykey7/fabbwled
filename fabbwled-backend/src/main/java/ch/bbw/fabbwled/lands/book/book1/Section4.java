9cde08abdc3363f11bb2f26c59e45f33c941c856
leo.debie <leo.debie@switch.ch>
2023-09-23 15:09:23 +0200

    began with section 4 but shard system and item system is required

--- fabbwled-backend/src/main/java/ch/bbw/fabbwled/lands/book/book1/Section4.java
+++ fabbwled-backend/src/main/java/ch/bbw/fabbwled/lands/book/book1/Section4.java
@@ -1,2 +1,52 @@
-package ch.bbw.fabbwled.lands.book.book1;public class Section4 {
+package ch.bbw.fabbwled.lands.book.book1;
+
+
+import ch.bbw.fabbwled.lands.book.SectionHandler;
+import ch.bbw.fabbwled.lands.book.SectionId;
+import ch.bbw.fabbwled.lands.book.SectionNode;
+import ch.bbw.fabbwled.lands.exception.FabledBusinessException;
+import ch.bbw.fabbwled.lands.service.PlayerSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class Section4 implements SectionHandler {
+    private final PlayerSession playerSession;
+
+    @Override
+    public SectionId getId() {
+        return new SectionId(1, 4);
+    }
+
+
+
+    @Override
+    public SectionNode getBody() {
+        return SectionNode.root().text("""
+                        The  priests  of  Alvir  and  Valmir  are  overjoyed  that  you  have returned the golden net.
+                                The high priest rewards you with 100 Shards  and  a  magic  weapon,  a  rune-engraved  trident. 
+                                Note  the weapon,  a  trident  (COMBAT  +1),  on  your  Adventure  Sheet. """)
+
+                .clickableTurnTo(1,220);
+    }
+
+    @Override
+    public void onClick(int id) {
+        var nextSection = switch (ClickOptions.values()[id]) {
+            case NEXT_SECTION -> {
+                List<String> currentPossessions = playerSession.getPlayer().possessions();
+                currentPossessions.add("trident");
+                playerSession.update(x -> x.withShards(x.shards().addShards(100)).withPossessions(currentPossessions).withBaseStats(x.baseStats().withCombat(x.baseStats().combat()+1)));
+                yield SectionId.book1(220);
+            }
+        };
+        playerSession.update(x -> x.withCurrentSection(nextSection));
+    }
+
+    enum ClickOptions {
+        NEXT_SECTION
+    }
 }
--- fabbwled-backend/src/main/java/ch/bbw/fabbwled/lands/book/book1/Section6.java
+++ fabbwled-backend/src/main/java/ch/bbw/fabbwled/lands/book/book1/Section6.java
@@ -1,2 +1,61 @@
-package ch.bbw.fabbwled.lands.book.book1;public class Section6 {
+package ch.bbw.fabbwled.lands.book.book1;
+
+
+
+
+import ch.bbw.fabbwled.lands.book.SectionHandler;
+import ch.bbw.fabbwled.lands.book.SectionId;
+import ch.bbw.fabbwled.lands.book.SectionNode;
+import ch.bbw.fabbwled.lands.exception.FabledBusinessException;
+import ch.bbw.fabbwled.lands.service.PlayerSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class Section6 implements SectionHandler {
+    private final PlayerSession playerSession;
+
+    @Override
+    public SectionId getId() {
+        return new SectionId(1, 6);
+    }
+
+
+
+    @Override
+    public SectionNode getBody() {
+        return SectionNode.root().text("""
+                        The chest springs open with a click.
+                        Inside you find 60 Shards, a mandolin  (CHARISMA  +1),  and  a  potion  of  healing.
+                        The potion can be used once, at any time (even in combat) to restore 5  Stamina  points.
+                        There  is  also  an  ancient  religious  text  about the  gods  of  the  Uttaku,  called  the  scroll  of  Ebron,
+                        which reveals that one of the gods of the Uttakin is called Ebron, and that he has fourteen angles.""")
+
+                .clickableTurnTo(4,10);
+    }
+
+    @Override
+    public void onClick(int id) {
+        var nextSection = switch (ClickOptions.values()[id]) {
+            case NEXT_SECTION -> {
+                playerSession.update(x -> x.withShards(x.shards().addShards(60)));
+                List<String> currentPossessions = playerSession.getPlayer().possessions();
+                List<String> currentPossessionsForHealing = playerSession.getPlayer().possessions();
+                currentPossessionsForHealing.add("potion of healing");
+                currentPossessions.add("mandolin");
+                playerSession.update(x -> x.withPossessions(currentPossessions).withBaseStats(x.baseStats().withCharisma(x.baseStats().charisma()+1)));
+                playerSession.update(x -> x.withPossessions(currentPossessionsForHealing));
+                yield SectionId.book1(10);
+            }
+        };
+        playerSession.update(x -> x.withCurrentSection(nextSection));
+    }
+
+    enum ClickOptions {
+        NEXT_SECTION
+    }
 }
+
