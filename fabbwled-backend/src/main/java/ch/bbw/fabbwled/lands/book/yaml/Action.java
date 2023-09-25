298a736a7d41640e05f92a51908e0b91f8017ff4
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2023-09-25 16:48:07 +0200

    Verify basic properties about sections (#391)
    ### What?
    
    This PR verifies basic properties about sections. Concretely, we check
    that every `choice` has at least two elements. If that's not upheld, we
    error.
    
    ### Why?
    
    To avoid mistakes.
    
    ### Testing?
    
    I have added two tests that test for errors. The existing YAMLs are a
    test that ensures that we don't get false positives.
    
    I am planning to add more complex session verification that makes sure
    that things like "no other action after `turnTo`" are upheld. (basically
    a compiler reachability analysis, this YAML section stuff is basically a
    compiler)

--- fabbwled-backend/src/main/java/ch/bbw/fabbwled/lands/book/yaml/Action.java
+++ fabbwled-backend/src/main/java/ch/bbw/fabbwled/lands/book/yaml/Action.java
@@ -1,18 +1,45 @@
 package ch.bbw.fabbwled.lands.book.yaml;
 
 import ch.bbw.fabbwled.lands.book.SectionId;
+import ch.bbw.fabbwled.lands.exception.FabledTechnicalException;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface Action {
-    record TextAction(String text) implements Action {}
+    void simpleVerify();
 
-    record IfAction(Condition condition, List<Action> then, Optional<List<Action>> else_) implements Action {}
+    record TextAction(String text) implements Action {
+        @Override
+        public void simpleVerify() {}
+    }
 
-    record TurnToAction(SectionId sectionId) implements Action {}
+    record IfAction(Condition condition, List<Action> then, Optional<List<Action>> else_) implements Action {
+        @Override
+        public void simpleVerify() {
+            then.forEach(Action::simpleVerify);
+            else_.ifPresent(else_ -> else_.forEach(Action::simpleVerify));
+        }
+    }
+
+    record TurnToAction(SectionId sectionId) implements Action {
+        @Override
+        public void simpleVerify() {}
+    }
 
     record Choice(List<SingleChoice> choices) implements Action {
+        @Override
+        public void simpleVerify() {
+            if (choices.isEmpty()) {
+                throw new FabledTechnicalException("Found choice with no choices, this doesn't make sense.");
+            }
+            if (choices.size() == 1) {
+                throw new FabledTechnicalException("Choice action only has a single choice, don't use a choice.");
+            }
+
+            choices.forEach(choice -> choice.actions.forEach(Action::simpleVerify));
+        }
+
         record SingleChoice(String text, List<Action> actions) {}
     }
 }
