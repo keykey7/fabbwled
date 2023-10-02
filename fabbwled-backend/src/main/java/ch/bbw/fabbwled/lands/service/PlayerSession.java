c27fad35e0e7dedee44d4198888afe25247dd846
MagischerWolf05 <gianwilhelm3@gmail.com>
2023-10-02 17:07:09 +0200

    feat: added the shardsystem to the validation

--- fabbwled-backend/src/main/java/ch/bbw/fabbwled/lands/service/PlayerSession.java
+++ fabbwled-backend/src/main/java/ch/bbw/fabbwled/lands/service/PlayerSession.java
@@ -57,7 +57,14 @@ public class PlayerSession {
             if (this.initialCreation) {
                 validateInitialCreation(playerDto);
             }
-
+            if (playerDto.shards().getShardCount() > player.shards().getShardCount()){
+                int ShardsAmount = playerDto.shards().getShardCount() - player.shards().getShardCount();
+                playerDto.shards.addShards(ShardsAmount);
+            }
+            if (playerDto.shards().getShardCount() < player.shards().getShardCount()){
+                int ShardsAmount = player.shards().getShardCount() - playerDto.shards().getShardCount();
+                playerDto.shards.addShards(ShardsAmount);
+            }
             if (playerDto.possessions().size() > 12) {
                 throw new FabledBusinessException("Character possession size not allowed over 12");
             }
