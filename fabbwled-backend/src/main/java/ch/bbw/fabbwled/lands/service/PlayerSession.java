3967907b410650a55cd590aa23456f6b58381f83
MagischerWolf05 <gianwilhelm3@gmail.com>
2023-10-02 16:23:04 +0200

    fix: added space ind the constructor

--- fabbwled-backend/src/main/java/ch/bbw/fabbwled/lands/service/PlayerSession.java
+++ fabbwled-backend/src/main/java/ch/bbw/fabbwled/lands/service/PlayerSession.java
@@ -18,7 +18,7 @@ import java.util.function.UnaryOperator;
 @SessionScope
 public class PlayerSession {
 
-	private PlayerDto player = new PlayerDto("Johanna Doe", SectionId.book1(15), Collections.emptySet(),new ShardSystem(16));
+	private PlayerDto player = new PlayerDto("Johanna Doe", SectionId.book1(15), Collections.emptySet(), new ShardSystem(16));
 
 	public void update(UnaryOperator<PlayerDto> modifier) {
 		player = modifier.apply(player);
@@ -27,6 +27,6 @@ public class PlayerSession {
 	@With
 	public record PlayerDto(String name,
 							SectionId currentSection,
-							Set<String> titlesAndHonours,ShardSystem shards) {
+							Set<String> titlesAndHonours, ShardSystem shards) {
 	}
 }
