b01f36f8199923db9b5cbffd3b5c2af6e3978f08
adrian schnyder <adrian.schnyder@econ.uzh.ch>
2023-11-13 15:40:10 +0100

    add hasProfession check

--- fabbwled-backend/src/main/java/ch/bbw/fabbwled/lands/book/yaml/Condition.java
+++ fabbwled-backend/src/main/java/ch/bbw/fabbwled/lands/book/yaml/Condition.java
@@ -1,5 +1,7 @@
 package ch.bbw.fabbwled.lands.book.yaml;
 
+import ch.bbw.fabbwled.lands.character.ProfessionEnum;
+
 public interface Condition {
     record HasTitle(String title) implements Condition {}
 
@@ -8,7 +10,9 @@ public interface Condition {
     record NeedsAtLeastShards(int amount) implements Condition {}
 
     record IsTickBoxDone(boolean isTickBoxDone) implements Condition {}
-  
+
     record HasPossession(String possession) implements Condition {}
-  
+
+    record HasProfession(ProfessionEnum profession) implements Condition {}
+
 }
