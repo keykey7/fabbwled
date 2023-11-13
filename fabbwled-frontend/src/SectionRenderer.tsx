fd604d8fbf02661129f009a8d90e806f483aad5d
Corsin <c0rr1t@noreply.github.com>
2023-11-13 16:26:58 +0100

    create `SectionRenderer` to render the API Response

---
+++ fabbwled-frontend/src/SectionRenderer.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+type SectionElementStyle = "NONE" | "SECTION" | "ITEM" | "ABILITY" | "TICKBOX";
+
+type Container = {
+  children: SectionElement[];
+  type: "CONTAINER";
+  style: SectionElementStyle;
+};
+
+type Simple = {
+  text: string;
+  type: "SIMPLE";
+  style: SectionElementStyle;
+};
+
+type Clickable = {
+  clickId: number;
+  text: string;
+  type: "CLICKABLE";
+  child: SectionElement;
+};
+
+type SectionElement = Simple | Clickable | Container;
+
+type OnClickCallBack = (clickId: Clickable["clickId"]) => void;
+
+export function convertToElement<T extends SectionElement>(
+  element: T,
+  onClick: OnClickCallBack,
+): React.ReactElement {
+  switch (element.type) {
+    case "CLICKABLE":
+      return (
+        <a onClick={() => onClick(element.clickId)}>
+          {convertToElement(element.child, onClick)}
+        </a>
+      );
+    case "SIMPLE":
+      return <p style={convertStyle(element.style)}>{element.text}</p>;
+    case "CONTAINER":
+      return (
+        <div style={convertStyle(element.style)}>
+          {element.children.map((el) => convertToElement(el, onClick))}
+        </div>
+      );
+    default:
+      return <></>;
+  }
+}
+
+export function convertStyle(style: SectionElementStyle): React.CSSProperties {
+  switch (style) {
+    case "NONE":
+      return {};
+    case "ITEM":
+      return { fontWeight: "bold" };
+    case "SECTION":
+      return { fontWeight: "bold" };
+    case "ABILITY":
+      return { textTransform: "uppercase" };
+    default:
+      return {};
+  }
+}
