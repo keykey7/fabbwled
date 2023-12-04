edd9a7c54930f058cc5d779c5658af296070600b
leo.debie <leo.debie@switch.ch>
2023-12-04 16:07:06 +0100

    fix formating issues

--- fabbwled-backend/src/main/java/ch/bbw/fabbwled/lands/book/book1/Section6.java
+++ fabbwled-backend/src/main/java/ch/bbw/fabbwled/lands/book/book1/Section6.java
@@ -29,12 +29,12 @@ public class Section6 implements SectionHandler {
     public SectionNode getBody() {
         return SectionNode.root().text("""
                         The chest springs open with a click.
-                        Inside you find 60 Shards, a mandolin  (CHARISMA  +1),  and  a  potion  of  healing.
+                        Inside you find 60 Shards, a mandolin  ( CHARISMA  +1 ),  and  a  potion  of  healing.
                         The potion can be used once, at any time (even in combat) to restore 5  Stamina  points.
                         There  is  also  an  ancient  religious  text  about the  gods  of  the  Uttaku,  called  the  scroll  of  Ebron,
                         which reveals that one of the gods of the Uttakin is called Ebron, and that he has fourteen angles.""")
 
-                .clickableTurnTo(4,10);
+                .clickableTurnTo(4, 10);
     }
 
     @Override
