5c2c706519b4b7a6449339636f5160bc83ee1d17
Oliver <75665514+Oliver-was-taken@users.noreply.github.com>
2023-12-11 16:52:32 +0100

    Section 618

---
+++ fabbwled-backend/src/main/java/ch/bbw/fabbwled/lands/book/book1/Section618.java
@@ -0,0 +1,31 @@
+package ch.bbw.fabbwled.lands.book.book1;
+
+import ch.bbw.fabbwled.lands.book.SectionHandler;
+import ch.bbw.fabbwled.lands.book.SectionId;
+import ch.bbw.fabbwled.lands.book.SectionNode;
+import ch.bbw.fabbwled.lands.character.PlayerDto;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+public class Section618 implements SectionHandler {
+    @Override
+    public SectionId getId() {
+        return SectionId.book1(618);
+    }
+
+    @Override
+    public SectionNode getBody(PlayerDto current) {
+        return SectionNode.root()
+                .text("""
+                        Becoming an initiate of Lacuna gives you the benefit of paying
+                        less for blessings and other services the temple can offer. It costs
+                        30 Shards to become an initiate. You cannot do this if you are
+                        already an initiate of another temple.
+                        """)
+                .activeIf(!current.god().isEmpty(), a -> a.text("Become an initiate of Lacuna")
+                        .choice(b -> b.text("Become an initiate and pay 30 Shards"), c -> c.clickable(playerDto -> playerDto.withShards(current.shards() - 30).withGod("Lacuna"), d -> d.text("Become an initiate and pay 30 Shards")))
+                        .clickableTurnTo(544));
+    }
+}
