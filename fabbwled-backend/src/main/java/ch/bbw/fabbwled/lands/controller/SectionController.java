c5ce9ee18549ab3f6b5d37e71239e0f9d364ad9b
lucasmillanm <70648632+lucasmillanm@users.noreply.github.com>
2024-01-08 16:19:40 +0100

    Marketplace first prototype (#950)
    ### What?
    Marketplace
    ### Why?
    For sections like #217 
    ### How?
    First, with existing Tools like choice and if cases. Afterwards we will
    iterate on it.
    ### Testing?
    
    ### Screenshots (for frontend changes)
    
    ### Anything Else?
    
    [//]: # (for a sample see:
    https://www.pullrequest.com/blog/writing-a-great-pull-request-description/)
    
    ---------
    
    Co-authored-by: miolmi <73534349+miolmi@users.noreply.github.com>
    Co-authored-by: Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>

--- fabbwled-backend/src/main/java/ch/bbw/fabbwled/lands/controller/SectionController.java
+++ fabbwled-backend/src/main/java/ch/bbw/fabbwled/lands/controller/SectionController.java
@@ -1,8 +1,13 @@
 package ch.bbw.fabbwled.lands.controller;
 
+import java.util.List;
+
 import ch.bbw.fabbwled.lands.book.SectionDto;
 import ch.bbw.fabbwled.lands.book.SectionId;
+import ch.bbw.fabbwled.lands.marketplace.MarketPlace;
+import ch.bbw.fabbwled.lands.service.MarketPlaceService;
 import ch.bbw.fabbwled.lands.service.SectionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 public class SectionController {
 
 	private final SectionService sectionService;
+	private final MarketPlaceService marketPlaceService;
 
 	/**
 	 * Peek at a book section without actually going there.
@@ -27,6 +33,13 @@ public class SectionController {
 	public SectionDto click(@RequestBody SectionClick click) {
 		return sectionService.onClick(click.clickId());
 	}
+
+    @GetMapping("/display-marketplace")
+    public List<MarketPlace> displayMarketplace(
+        @RequestBody @Valid List<MarketPlace> marketPlaceValues
+    ) {
+        return this.marketPlaceService.displayMarketplace(marketPlaceValues);
+    }
 
 	public record SectionClick(int clickId) {
 	}
