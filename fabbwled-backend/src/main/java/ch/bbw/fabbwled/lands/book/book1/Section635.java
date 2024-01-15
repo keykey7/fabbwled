14126e70ca94a4e089d302889418429f19ae1227
Oliver <75665514+Oliver-was-taken@users.noreply.github.com>
2024-01-15 15:47:54 +0100

    Section 635

---
+++ fabbwled-backend/src/main/java/ch/bbw/fabbwled/lands/book/book1/Section635.java
@@ -0,0 +1,26 @@
+package ch.bbw.fabbwled.lands.book.book1;
+
+import ch.bbw.fabbwled.lands.book.SectionHandler;
+import ch.bbw.fabbwled.lands.book.SectionId;
+import ch.bbw.fabbwled.lands.book.SectionNode;
+import ch.bbw.fabbwled.lands.character.PlayerDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Section635 implements SectionHandler {
+
+    @Override
+    public SectionId getId() {
+        return SectionId.book1(635);
+    }
+
+    @Override
+    public SectionNode getBody(PlayerDto current) {
+        return SectionNode.root()
+                .text("""
+                        If the box above is empty, put a tick in it now and turn to 80 . If
+                        it is already ticked, turn to 470 .
+                        """)
+                .clickableTurnTo(470);
+    }
+}
